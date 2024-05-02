51bbfa2c5e5c32a343621b3b00a350fc567ee8e6
sehabzks <99829809+sehabzks@users.noreply.github.com>
2024-05-02 14:31:48 +0300

    .

---
+++ src/main/java/com/example/dockerOdev/Controller.java
@@ -0,0 +1,19 @@
+
+package com.example.dockerOdev;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
+    @GetMapping
+    public String deneme2(){
+        return "123123312";
+    }
+
+
+}
